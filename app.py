15f7d2c6bc9c856a9dee93f70f1bc7f6320cafc3
Lesedi <molemilesedi@gmail.com>
2025-12-20 22:55:23 +0200

    chore: refactor

--- app.py
+++ app.py
@@ -1,19 +1,28 @@
 import yfinance as yf
 
+
+def stringify_keys(d):
+    return {str(k): v for k, v in d.items()}
+
+
 def handler(event, context):
     ticker = event.get("ticker")
     statement_type = event.get("statement")
-    
+
     if not ticker:
-        return {"message": "No ticket provided"}
-    
+        return {
+            "message": "No ticket provided"
+        }
+    elif not statement_type:
+        return {
+            "message": "No statement type provided"
+        }
+
     stock = yf.Ticker(ticker)
-    
-    def stringify_keys(d):
-        return {str(k): v for k, v in d.items()}
-    
+
     if statement_type == "balance_sheet":
-        last_quarter_date, last_quarter_data = next(iter(stock.get_balance_sheet(as_dict=True, pretty=True, freq="quarterly").items()), (None, None))
+        last_quarter_date, last_quarter_data = next(iter(stock.get_balance_sheet(
+            as_dict=True, pretty=True, freq="quarterly").items()), (None, None))
         balance_sheet_data = stock.get_balance_sheet(as_dict=True, pretty=True)
 
         return {
@@ -21,22 +30,26 @@ def handler(event, context):
             "lastQuarter": {"date": str(last_quarter_date), "data": last_quarter_data}
         }
     elif statement_type == "cash_flow":
-        last_quarter_date, last_quarter_data = next(iter(stock.get_cash_flow(as_dict=True, pretty=True, freq="quarterly").items()), (None, None))
-        cash_flow_statement_data = stock.get_cash_flow(as_dict=True, pretty=True)
-        
+        last_quarter_date, last_quarter_data = next(iter(stock.get_cash_flow(
+            as_dict=True, pretty=True, freq="quarterly").items()), (None, None))
+        cash_flow_statement_data = stock.get_cash_flow(
+            as_dict=True, pretty=True)
+
         return {
             "data": stringify_keys(cash_flow_statement_data),
             "lastQuarter": {"date": str(last_quarter_date), "data": last_quarter_data}
         }
     elif statement_type == "income_statement":
-        last_quarter_date, last_quarter_data = next(iter(stock.get_income_stmt(as_dict=True, freq="quarterly", pretty=True).items(), (None, None)))
-        income_statement_data = stock.get_income_stmt(as_dict=True, pretty=True)
+        last_quarter_date, last_quarter_data = next(iter(stock.get_income_stmt(
+            as_dict=True, freq="quarterly", pretty=True).items()), (None, None))
+        income_statement_data = stock.get_income_stmt(
+            as_dict=True, pretty=True)
 
         return {
             "data": stringify_keys(income_statement_data),
             "lastQuarter": {"date": str(last_quarter_date), "data": last_quarter_data}
         }
-    
+
     return {
         "message": "Incorrect statement type was passed."
     }
